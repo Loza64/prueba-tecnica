3f8a4e8590cf7405e993c39d7847447a84a0ec98
Roberto Loza <robertoloxa88@gmail.com>
2025-05-11 08:47:31 -0600

    fix import path

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,8 +3,8 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entity/user.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/data/entity/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
